3e26fe7b16f141436bf52cbfe72dcfb4183c454d
vinayakgarole <vinayakgarole10@gamil.com>
2023-02-06 18:02:57 +0530

    Added: Created simple linkedlist of 56, 30 and 70

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -5,5 +5,18 @@ public class LinkedList {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Linked List Program");
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 }
